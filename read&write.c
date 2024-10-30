98a626f598ad082a5aab933ce1a1547201934635
timosdev99 <timothyisah4@gmail.com>
2024-10-30 16:48:25 +0100

    learnt

--- read&write.c
+++ read&write.c
@@ -43,7 +43,7 @@ for (row = 0; row <= 4; row++)
 {
 for (column = 0; column <=4; column++)
 {
-my_array[row][column] = 0;
+my_array[row][column] = 0;  
 printf ("%d ", my_array[row][column]);
 }
 printf ("\n");
